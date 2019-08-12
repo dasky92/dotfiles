b7a11b8bc63191cb467a36e8a53b281d794613e0
root <root@localhost.localdomain>
2019-08-12 19:09:17 +0800

    add install_vimrc

--- sys_init.sh
+++ sys_init.sh
@@ -7,18 +7,43 @@
 # add 'BOOT=yes DNS1=8.8.8.8 DNS2=4.2.2.2'
 
 # install development dependency
-yum update
-
-yum install git vim
-yum install zlib zlib-devel bzip2 bzip2-devel openssl openssl-devel libffi libffi-devel gcc gcc-c++ libstdc++-devel
+# yum update
+INSTALL_LIB(){
+    yum install git vim
+    yum install zlib zlib-devel bzip2 bzip2-devel openssl openssl-devel libffi libffi-devel gcc gcc-c++ libstdc++-devel
+}
 
 INSTALL_PYTHON3(){
     echo "start install python3..."
+    wget https://www.python.org.ftp.python/3.7.4/Python-3.7.4.tgz
+    tar -xvf Python-3.7.4.tgz
+    cd Python-3.7.4
+    mkdir -p /usr/local/python3
+    ./configure --prefix=/usr/local/python3
+    make && make install
+    ln -s /usr/local/pythons/python3 /usr/bin/python3
+    python3 --version
     echo "python3 installed."
 } 
 
 INSTALL_VIMRC(){
     echo "start install vimrc..."
+    mkdir ~/.vim/colors
+    cd ~
+    git clone https://github.com/wanchaosoft/daevim.git ./.vim
+    ln -s ./.vim/.vim.local ./.vimrc
     echo "vimrc installed."
 }
 
+MAIN(){
+    INSTALL_LIB
+    yum update
+    cd /opt
+    INSTALL_PYTHON3
+    cd /opt
+    INSTALL_VIMRC
+    cd /opt
+}
+
+# execute it
+# MAIN
