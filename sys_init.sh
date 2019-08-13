c2f1ff72698d3c0898df8870fade0be35de7fb00
wanchaosoft <Daedrath@outlook.com>
2019-08-13 17:42:44 +0800

    true python url

--- sys_init.sh
+++ sys_init.sh
@@ -15,7 +15,7 @@ INSTALL_LIB(){
 
 INSTALL_PYTHON3(){
     echo "start install python3..."
-    wget https://www.python.org.ftp.python/3.7.4/Python-3.7.4.tgz
+    wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
     tar -xvf Python-3.7.4.tgz
     cd Python-3.7.4
     mkdir -p /usr/local/python3
