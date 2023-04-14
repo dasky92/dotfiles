286a6ff796222825c1d41eb2d3ae1378aa80d5a8
wanchao <dasky92@163.com>
2023-04-14 13:16:31 +0800

    feat: update

--- scripts/brew_install.sh
+++ scripts/brew_install.sh
@@ -4,7 +4,7 @@ if [ "$(command -v brew)" == "" ]; then
   # install brew for mac
   /bin/sh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
 else
-  echo "brew is already installed."
+  echo "brew is already installed. Continue..."
 fi
 
 
